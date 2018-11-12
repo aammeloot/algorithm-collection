112be208bbd47e1c0cf037e5463a39885c29dba1
Aurelien Ammeloot <aammeloot@gmail.com>
2018-11-12 14:11:54 +0000

    Added comments

--- count-occurrences.py
+++ count-occurrences.py
@@ -5,11 +5,11 @@
 usernames = ["neil", "emily", "scott", "emily", "scott", "emily", "scott", "neil","neil","neil","neil","neil","neil","neil","neil","neil","neil","scott", "emily","neil","neil","neil","neil", "emily", "scott"]
 
 search_term = "neil" # We will count "neil"
-count = 0
+count = 0 # At first there is nothing found
 
 # Go through all usernames
-for e in usernames:
-    if e == search_term:
-        count += 1
+for e in usernames: # Iterate through the list
+    if e == search_term: 
+        count += 1 # If match add one to count
 
 print(search_term, "appears", count, "times.")
