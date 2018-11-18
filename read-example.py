33f5842d428f314d64d85365606195da718e6f4b
Aurélien Ammeloot <aurelienammeloot@Aureliens-Air.lan>
2018-11-18 21:26:57 +0000

    Example of string read

--- read-example.py
+++ read-example.py
@@ -1,6 +1,23 @@
 # Reading example
 
-f = open("text1.txt")
-text_content = f.read()
-print(text_content)
+# Read the whole file 
+f = open("text1.txt")  # Open a text file in default read mode
+text_content = f.read() # Read the whole file and store in a variable
+print(text_content) # Print the whole text to screen
+f.close() # Close the file
 
+# Read line by line
+f = open("text1.txt")
+print()
+print("First 4 lines:")
+print()
+for n in range(4): # Repeat 4 times
+    print(f.readline()) # Print current line and move on to next
+f.close()
+
+# Create a list with all lines
+f = open("text1.txt")
+all_lines = f.readlines() # Read all lines and save in a list
+f.close()
+
+print(all_lines) # print the list of lines
